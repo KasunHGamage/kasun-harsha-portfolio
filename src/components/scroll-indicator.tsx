5671575df1f7dd059b04fd48bc6f95478fc87a4a
kasun harsha <kasunharshagamage@gmail.com>
2025-12-16 17:55:33 +0000

    take this little bit up (_for element <ScrollIndicator>_)

--- src/components/scroll-indicator.tsx
+++ src/components/scroll-indicator.tsx
@@ -6,7 +6,7 @@ import { Mouse } from 'lucide-react';
 export default function ScrollIndicator() {
   return (
     <motion.div
-      className="absolute bottom-10 left-1/2 -translate-x-1/2"
+      className="absolute bottom-20 left-1/2 -translate-x-1/2"
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 1.5, duration: 0.5 }}

69c4228751e202248892c842eda63ac7916f597b
kasun harsha <kasunharshagamage@gmail.com>
2025-12-16 18:51:06 +0000

    Remove the forced page refresh that happens when navigating between sect

--- src/app/inner-layout.tsx
+++ src/app/inner-layout.tsx
@@ -1,14 +1,12 @@
 'use client';
 
 import { Toaster } from "@/components/ui/toaster"
-import { usePageReloader } from '@/hooks/use-page-reloader';
 
 export default function InnerLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    usePageReloader();
 
     return (
         <>
